cf2cf8ac2ce5d73ee481700d60d09808b9952d13
Poorna Chandu Kottali <poornachandukottali@gmail.com>
2026-06-24 17:08:51 +0530

    Initial commit - AI Job Matcher V1

---
+++ README.md
@@ -0,0 +1,18 @@
+# AI Job Matcher
+
+A Python-based web application that compares a resume with a job description.
+
+## Features
+
+- Resume PDF Upload
+- Skill Detection
+- Job Skill Detection
+- Match Score Calculation
+- Missing Skills Identification
+- Learning Suggestions
+
+## Technologies Used
+
+- Python
+- Streamlit
+- pdfplumber
---
+++ app.py
@@ -0,0 +1,95 @@
+import streamlit as st
+import pdfplumber
+
+st.title("AI Job Matcher")
+
+st.write("Upload your resume and compare it with a job description.")
+
+resume = st.file_uploader("Upload Your Resume", type=["pdf"])
+
+job_description = st.text_area("Paste Job Description Here")
+
+if resume:
+
+    st.success("Resume uploaded successfully!")
+
+    text = ""
+
+    with pdfplumber.open(resume) as pdf:
+        for page in pdf.pages:
+            extracted_text = page.extract_text()
+            if extracted_text:
+                text += extracted_text
+
+    skills = [
+        "Python",
+        "SQL",
+        "Git",
+        "AWS",
+        "Docker",
+        "C",
+        "MATLAB",
+        "Xilinx Vivado",
+        "Cadence"
+    ]
+
+    # Resume Skills
+    detected_skills = []
+
+    for skill in skills:
+        if skill.lower() in text.lower():
+            detected_skills.append(skill)
+
+    st.subheader("Detected Resume Skills")
+
+    for skill in detected_skills:
+        st.write("✅", skill)
+
+    # Job Description Skills
+    job_skills = []
+
+    for skill in skills:
+        if skill.lower() in job_description.lower():
+            job_skills.append(skill)
+
+    st.subheader("Job Skills")
+
+    for skill in job_skills:
+        st.write("📌", skill)
+    matched_skills = []
+
+    for skill in job_skills:
+        if skill in detected_skills:
+            matched_skills.append(skill)
+
+    if len(job_skills) > 0:
+        match_score = (len(matched_skills) / len(job_skills)) * 100
+    else:
+        match_score = 0
+
+    st.subheader("Match Score")
+
+    st.write(f"{match_score:.0f}%")
+    missing_skills = []
+
+    for skill in job_skills:
+        if skill not in detected_skills:
+            missing_skills.append(skill)
+
+    st.subheader("Missing Skills")
+
+    for skill in missing_skills:
+        st.write("❌", skill)
+    st.subheader("Matched Skills")
+
+    for skill in matched_skills:
+        st.write("✅", skill)
+
+    st.subheader("Suggestions")
+
+    for skill in missing_skills:
+        st.write(f"💡 Learn {skill} fundamentals")
+
+    st.subheader("Resume Text")
+
+    st.write(text)
